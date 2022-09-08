e5faee8fbd501cc3d41c6d9dc01aa7dc35d7e8a5
jeelkanani <80504844+jeelkanani@users.noreply.github.com>
2022-09-08 21:00:55 +0530

    Added security with custom users jdbc authentication using H2 database.

--- studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
+++ studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/config/SecurityConfig.java
@@ -16,16 +16,32 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-      
+        @Autowired
+        DataSource dataSource;
+
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-                auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN");
+        
+                auth.jdbcAuthentication().dataSource(dataSource)
+                        .withDefaultSchema()
+                        .withUser("admin")
+                        .password("pass")
+                        .roles("ADMIN", "USER")
+                        .and()
+                        .withUser("user")
+                        .password("pass")
+                        .roles("USER");
+                // auth.inMemoryAuthentication()
+                // .withUser("admin")
+                // .password("admin")
+                // .roles("ADMIN");
         } 
 
-
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            http.headers().frameOptions().disable();
+        }
         // provide encoding without any opration this method automatic called by spring boot
         @Bean
         public PasswordEncoder getPasswordEncoder()
